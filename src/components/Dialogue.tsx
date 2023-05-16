e819c6bddf98df19af55e9fd01b291f03fc0ae69
Ke Duong <kelyduong@gmail.com>
2023-05-16 22:57:42 +1000

    Initial Setup

---
+++ src/components/Dialogue.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent, useState } from "react";
+
+import { Configuration, OpenAIApi } from "openai";
+
+function Dialogue() {
+  const [prompt, setPrompt] = useState("");
+  const;
+
+  function updatePrompt(event: ChangeEvent<HTMLInputElement>) {
+    setPrompt(event.target.value);
+  }
+
+  return (
+    <div>
+      <div className="text-4xl text-red-600">Chatbot!</div>
+      <div>
+        <input
+          className="shadow appearance-none border rounded w-max py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="prompt"
+          type="text"
+          value={prompt}
+          onChange={updatePrompt}
+        ></input>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline max-w-fit max-h-fit"
+          type="button"
+          onClick={() => console.log(prompt)}
+        >
+          Click
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Dialogue;
