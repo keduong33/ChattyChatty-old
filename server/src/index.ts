e4550f8d888c5a73f81038ff53e6e042a80fe192
Ke Duong <kelyduong@gmail.com>
2023-06-04 23:35:05 +1000

    (fe,be) successfully establish basic be & fe connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,35 @@
-import * as http from "http";
+import http, { IncomingMessage, ServerResponse } from "http";
 import "dotenv/config";
 
-const hostname = "127.0.0.1";
+const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-});
+const server = http.createServer(
+  (request: IncomingMessage, response: ServerResponse) => {
+    switch (request.url) {
+      case "/":
+        if (request.method == "GET") {
+          response.end("Hello bye everyone");
+        }
+        break;
+      case "/test":
+        if (request.method == "GET") {
+          console.log("got here");
+          response.setHeader("Access-Control-Allow-Origin", "*");
+          response.setHeader("Access-Control-Allow-Methods", "GET");
+          response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+          response.end(JSON.stringify("We are connected"));
+        }
+        break;
+
+      default:
+        response.statusCode = 404;
+        response.end();
+        break;
+    }
+  }
+);
 
 server.listen(port, hostname, () => {
-  console.log(
-    `Server running at http://${hostname}:${port}/${process.env.API_KEY}`
-  );
+  console.log(`Server running at http://${hostname}:${port}`);
 });
