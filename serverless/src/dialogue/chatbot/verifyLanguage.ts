ddfe3de20ba9c700ac5c4e1eb3061bfef36c07fb
Ke Duong <kelyduong@gmail.com>
2023-06-24 13:56:59 +1000

    (be) remove supported languages

--- serverless/src/dialogue/chatbot/verifyLanguage.ts
+++ serverless/src/dialogue/chatbot/verifyLanguage.ts
@@ -1,4 +1,4 @@
-export const allowedLanguages = ["Deutsch", "Italian", "English"];
+export const allowedLanguages = ["English"];
 
 export function isValidLanguage(language: string): boolean {
   return allowedLanguages.includes(language);
