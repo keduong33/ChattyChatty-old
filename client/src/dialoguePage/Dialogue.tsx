293a0958a03e71272dee6f3640493ac3f0aee0d6
Ke Duong <kelyduong@gmail.com>
2023-06-25 22:41:11 +1000

    (fe) add disclaimer

--- client/src/dialoguePage/Dialogue.tsx
+++ client/src/dialoguePage/Dialogue.tsx
@@ -7,9 +7,14 @@ import { UserInputForm } from "./UserInputForm";
 
 export function DialoguePage() {
   return (
-    <div>
+    <div className="max-w-sm">
       <div title="Page Title" className="text-4xl text-red-600">
         Chatbot!
+      </div>
+
+      <div className="">
+        <b>Disclaimer:</b> First time sending the text or recording your voice,
+        the servers (HuggingFace will be a bit slow)
       </div>
 
       <LanguagePicker />
