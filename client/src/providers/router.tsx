1659a639d186814c489cf6a719084216aa9fddaf
Ke Duong <kelyduong@gmail.com>
2023-06-25 14:05:30 +1000

    (fe) rename the files

--- client/src/providers/router.tsx
+++ client/src/providers/router.tsx
@@ -2,7 +2,7 @@ import {
   createBrowserRouter,
   RouterProvider as RRProvider,
 } from "react-router-dom";
-import { DialoguePage } from "../dialoguePage/test";
+import { DialoguePage } from "../dialoguePage/Dialogue";
 
 const router = createBrowserRouter([
   {
